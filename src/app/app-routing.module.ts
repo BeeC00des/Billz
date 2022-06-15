62ff119ed62b276d701d6b8e01e43897d7aa6da8
Ajayi bolarinwa <ajayibcd@gmail.com>
2022-06-15 06:36:50 -0700

    added layout of billing project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsFormComponent } from './details-form/details-form.component';
+import { PaymentFormComponent } from './payment-form/payment-form.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path:'Details', component: DetailsFormComponent},
+  { path:'Payment', component:PaymentFormComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsFormComponent } from './details-form/details-form.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { PaymentFormComponent } from './payment-form/payment-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsFormComponent,
+    SideBarComponent,
+    PaymentFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
